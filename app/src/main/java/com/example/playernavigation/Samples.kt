cf1cf83333b8eb0be76c666b11fb4842dcaf4781
SirWaithaka <kennwaithaka@gmail.com>
2019-03-20 14:03:34 +0300

    Added video play functionality test

---
+++ app/src/main/java/com/example/playernavigation/Samples.kt
@@ -0,0 +1,5 @@
+package com.example.playernavigation
+
+import android.net.Uri
+
+data class Samples(var MEDIA_URI: Uri = Uri.parse("asset:///thor_ragnarok.mp4"))
--- app/src/main/java/com/example/playernavigation/ui/VideoFragment.kt
+++ app/src/main/java/com/example/playernavigation/ui/VideoFragment.kt
@@ -10,9 +10,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.playernavigation.R
+import com.example.playernavigation.Samples
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 class VideoFragment : Fragment() {
    private val TAG = "VideoFragment"
+
+   private lateinit var player: SimpleExoPlayer
+   private lateinit var playerView: PlayerView
 
    override fun onAttach(context: Context?) {
       super.onAttach(context)
@@ -25,17 +37,38 @@ class VideoFragment : Fragment() {
    }
 
    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-      return inflater.inflate(R.layout.fragment_video, container, false)
+      val videoFragmentView = inflater.inflate(R.layout.fragment_video, container, false)
+//      playerView = videoFragmentView.findViewById(R.id.video_view)
+      return videoFragmentView
    }
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
+
+      playerView = view.findViewById(R.id.video_view)
       Log.d(TAG, "onViewCreated: video fragment")
    }
 
    override fun onActivityCreated(savedInstanceState: Bundle?) {
       super.onActivityCreated(savedInstanceState)
       Log.d(TAG, "onActivityCreated: video fragment")
+   }
+
+   override fun onStart() {
+      super.onStart()
+      Log.d(TAG, "onStart: video fragment")
+
+      player = ExoPlayerFactory.newSimpleInstance(this.context, DefaultTrackSelector())
+
+      playerView.player = player
+      val sample = Samples()
+      val dataSourceFactory = DefaultDataSourceFactory(this.context, Util.getUserAgent(this.context, "youtise-player"))
+      val mediaSource = ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(sample.MEDIA_URI)
+
+      player.prepare(mediaSource)
+
+      player.addListener(PlayBackStateChanged(this))
+      player.playWhenReady = true
    }
 
    override fun onResume() {
@@ -48,10 +81,10 @@ class VideoFragment : Fragment() {
          apply()
       }
 
-      Handler().postDelayed({
-         Log.d(TAG, "image timeout")
+      /*Handler().postDelayed({
+         Log.d(TAG, "video timeout")
          this.findNavController().popBackStack()
-      }, 10000)
+      }, 10000)*/
    }
 
    override fun onPause() {
@@ -62,6 +95,9 @@ class VideoFragment : Fragment() {
    override fun onStop() {
       super.onStop()
       Log.d(TAG, "onStop: video fragment")
+
+      playerView.player = null
+      player.release()
    }
 
    override fun onDestroyView() {
@@ -77,5 +113,18 @@ class VideoFragment : Fragment() {
    override fun onDetach() {
       super.onDetach()
       Log.d(TAG, "onDetach: video fragment")
+   }
+
+   inner class PlayBackStateChanged (val fragment: Fragment): Player.EventListener {
+      override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+         when (playbackState) {
+            Player.STATE_ENDED -> {
+               Log.d(TAG, "video has finished playing")
+
+               fragment.findNavController().popBackStack()
+            }
+            else -> super.onPlayerStateChanged(playWhenReady, playbackState)
+         }
+      }
    }
 }
