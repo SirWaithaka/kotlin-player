4570f6f50cb80cddf6a0af8b5f949d15d95e4826
SirWaithaka <kennwaithaka@gmail.com>
2019-05-22 14:15:30 +0300

    Add function when user navigates back from LocationsFragment, check if input is valid email and activate button; Navigate to next route only when locations have been fetched; Show loading text on button when app is busy fetching locations

--- app/src/main/java/com/example/player/ui/fragments/EmailFragment.kt
+++ app/src/main/java/com/example/player/ui/fragments/EmailFragment.kt
@@ -1,10 +1,12 @@
 package com.example.player.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.player.R
@@ -20,9 +22,7 @@ import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
 
 class EmailFragment : Fragment(), KodeinAware {
-   companion object {
-//      const val TAG = "EmailFragment"
-   }
+   private val TAG = "EmailFragment"
 
    override val kodein: Kodein by kodein()
    private val viewModelFactory: ViewModelFactory by instance()
@@ -31,11 +31,6 @@ class EmailFragment : Fragment(), KodeinAware {
    private lateinit var submitButton: MaterialButton
    private lateinit var viewModel: LocationsViewModel
 
-   private val onEmailSubmitListener = View.OnClickListener {
-      val userEmail: String = emailInput.text.toString()
-      viewModel.validateEmail(userEmail)
-      this.findNavController().navigate(R.id.destination_locations)
-   }
    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
       return inflater.inflate(R.layout.fragment_email, container, false)
    }
@@ -43,17 +38,39 @@ class EmailFragment : Fragment(), KodeinAware {
    override fun onActivityCreated(savedInstanceState: Bundle?) {
       super.onActivityCreated(savedInstanceState)
       viewModel = ViewModelProviders.of(activity!!, viewModelFactory).get(LocationsViewModel::class.java)
+
+      emailInput = view!!.findViewById(R.id.textInput_email)
+      submitButton = view!!.findViewById(R.id.mButton_email)
    }
    override fun onResume() {
       super.onResume()
+      // check if there is a value in emailInput and change
+      // submit button to enabled
+      emailInput.text.apply {
+         submitButton.isEnabled = this.toString().isValidEmail()
+      }
 
-      emailInput = view!!.findViewById(R.id.textInput_email)
       // onTextChanged is a custom extension to TextInput
       emailInput.onTextChanged {
-         val userEmail = it
-         submitButton.isEnabled = userEmail.isValidEmail()
+         submitButton.text = getString(R.string.submit_email)
+         submitButton.isEnabled = it.isValidEmail()
       }
-      submitButton = view!!.findViewById(R.id.mButton_email)
-      submitButton.setOnClickListener(onEmailSubmitListener)
+      submitButton.setOnClickListener(OnEmailSubmitListener())
+      viewModel.hasValidatedEmail.observe(this, Observer {
+         if (it.getContent() == true) findNavController().navigate(R.id.destination_locations)
+      })
+   }
+
+   /*
+    * When submit button is clicked submit value to view model
+    * Disable button
+    */
+   private inner class OnEmailSubmitListener: View.OnClickListener {
+      override fun onClick(v: View) {
+         val userEmail: String = this@EmailFragment.emailInput.text.toString()
+         this@EmailFragment.viewModel.submitEmail(userEmail)
+         this@EmailFragment.submitButton.isEnabled = false
+         this@EmailFragment.submitButton.text = getString(R.string.loading)
+      }
    }
 }
