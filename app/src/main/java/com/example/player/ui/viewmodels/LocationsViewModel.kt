647f6d101cc77b83fd78cd47bab2d4394baca501
SirWaithaka <kennwaithaka@gmail.com>
2019-05-22 14:36:21 +0300

    Change function name that validates email and requests fetch of locations Change how submitEmail function handles fetch of locations Add new livedata object; hasValidatedEmail -> For use by EmailFragment Add data class that handles storage of credentials

--- app/src/main/java/com/example/player/ui/viewmodels/LocationsViewModel.kt
+++ app/src/main/java/com/example/player/ui/viewmodels/LocationsViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.player.ui.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,7 @@ import com.example.player.data.db.entities.Location
 import com.example.player.data.db.models.DeviceModel
 import com.example.player.data.repository.DeviceRepository
 import com.example.player.data.repository.LocationsRepository
+import com.example.player.internal.SingleEvent
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
@@ -14,21 +16,20 @@ class LocationsViewModel(
       private val locationsRepository: LocationsRepository,
       private val deviceModel: DeviceModel,
       deviceRepository: DeviceRepository
-) : ViewModel(), CoroutineScope {
+) : ScopedViewModel(), CoroutineScope {
 
-//   private val Tag = "LocationsViewModel"
-   private val job = Job()
+   private val Tag = "LocationsViewModel"
+
+   private val credentials: Credentials = Credentials()
    private lateinit var mSelectedLocation: Location
    private lateinit var mPassword: String
-   private var mLocations = MutableLiveData<List<Location>>()
-   val locations: LiveData<List<Location>>
-         get() = mLocations
-   override val coroutineContext: CoroutineContext
-         get() = job + Dispatchers.IO
 
+   private var mLocations = MutableLiveData<List<Location>>()
+   val locations: LiveData<List<Location>> get() = mLocations
    private val mHasAuthenticated = MutableLiveData<Boolean>()
-   val hasAuthenticated: LiveData<Boolean>
-         get() = mHasAuthenticated
+   val hasAuthenticated: LiveData<Boolean> get() = mHasAuthenticated
+   private val mHasValidatedEmail = MutableLiveData<SingleEvent<Boolean>>()
+   val hasValidatedEmail: LiveData<SingleEvent<Boolean>> get() = mHasValidatedEmail
 
    init {
       deviceRepository.setOnAuthStatusChangedListener(object: DeviceRepository.AuthenticationStatusListener {
@@ -40,14 +41,21 @@ class LocationsViewModel(
       locationsRepository.setOnLocationsFetchedListener(object: LocationsRepository.LocationsFetchedListener {
          override fun onLocationsFetched(locations: List<Location>) {
             mLocations.postValue(locations)
+            mHasValidatedEmail.postValue(SingleEvent(true))
+            Log.d(Tag, "Locations have been fetched")
          }
       })
    }
 
-   fun validateEmail(userEmail: String) {
+   fun submitEmail(userEmail: String) {
+      credentials.email = userEmail
       launch {
-         locationsRepository.getLocationsByEmail(userEmail)
-         deviceModel.locationEmail = userEmail
+         if (userEmail != deviceModel.locationEmail)
+            locationsRepository.validateEmail(userEmail)
+         else {
+            locationsRepository.getLocations(userEmail)
+            deviceModel.locationEmail = userEmail
+         }
       }
    }
    fun setSelectedLocation(selectedLocation: Location) {
@@ -75,4 +83,10 @@ class LocationsViewModel(
    fun getAuthenticationStatus(): Boolean {
       return deviceModel.authStatus
    }
+
+   data class Credentials(
+      var email: String = "",
+      var locationId: String = "",
+      var password: String = ""
+   )
 }
