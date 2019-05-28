30e85aece0c706ff56c455cad7967729122f5157
SirWaithaka <kennwaithaka@gmail.com>
2019-05-28 13:14:38 +0300

    Change 'fitSystemWindows' property for activity phone; Add leanback support for phone-> Hide status bar and navigation bar

--- app/src/main/java/com/youtise/player/ui/LeanbackActivity.kt
+++ app/src/main/java/com/youtise/player/ui/LeanbackActivity.kt
@@ -3,7 +3,6 @@ package com.youtise.player.ui
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
@@ -11,21 +10,21 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.tapadoo.alerter.Alerter
 import com.youtise.player.R
 import com.youtise.player.ui.viewmodels.ErrorsViewModel
 import com.youtise.player.ui.viewmodels.SettingsViewModel
 import com.youtise.player.ui.viewmodels.ViewModelFactory
-import com.google.android.material.navigation.NavigationView
-import com.tapadoo.alerter.Alerter
 import kotlinx.android.synthetic.main.activity_main.*
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
 
-class MainActivity : AppCompatActivity(), KodeinAware {
+class LeanbackActivity: AppCompatActivity(), KodeinAware {
 
-   private val TAG = "PlayerMainActivity"
+   //   private val TAG = "PlayerMainActivity"
 
    override val kodein: Kodein by kodein()
    private val viewModelFactory: ViewModelFactory by instance()
@@ -34,26 +33,9 @@ class MainActivity : AppCompatActivity(), KodeinAware {
    private lateinit var drawerNavView: NavigationView
    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
 
-   private val uiVisibilityFlags = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-         View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-         View.SYSTEM_UI_FLAG_FULLSCREEN)
-
-
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_main)
-
-      // hide navigation and status bars
-      window.decorView.apply {
-         systemUiVisibility = uiVisibilityFlags
-         this.setOnSystemUiVisibilityChangeListener {
-            if ((it and View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
-               systemUiVisibility = uiVisibilityFlags
-         }
-      }
+      setContentView(R.layout.activity_leanback)
 
       errorsViewModel = ViewModelProviders.of(this, viewModelFactory).get(ErrorsViewModel::class.java)
       settingsViewModel = ViewModelProviders.of(this, viewModelFactory).get(SettingsViewModel::class.java)
@@ -90,20 +72,7 @@ class MainActivity : AppCompatActivity(), KodeinAware {
 
    override fun onSupportNavigateUp() =
       Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp() ||
-      super.onSupportNavigateUp()
-
-   override fun onWindowFocusChanged(hasFocus: Boolean) {
-      super.onWindowFocusChanged(hasFocus)
-      if (hasFocus) {
-         window.decorView.systemUiVisibility = uiVisibilityFlags
-      }
-   }
-
-   override fun onDestroy() {
-      super.onDestroy()
-      window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-      window.decorView.setOnSystemUiVisibilityChangeListener(null)
-   }
+            super.onSupportNavigateUp()
 
    private fun handleDrawerActions(resourceId: Int) {
 
